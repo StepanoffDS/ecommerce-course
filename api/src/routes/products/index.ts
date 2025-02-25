41136b74885114cf0c86590495f12c7f65e9525d
stepanoff dmitrii <stepanovdimamain@gmail.com>
2025-02-25 12:11:43 +0400

    zod validation

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,6 +6,9 @@ import {
 	getProductById,
 	updateProduct,
 } from './products.controller';
+import { validateData } from '../../middlewares/validation.middleware';
+
+import { createProductSchema, updateProductSchema } from '../../db/schema';
 
 const router = Router();
 
@@ -13,9 +16,9 @@ router.get('/', getAllProducts);
 
 router.get('/:id', getProductById);
 
-router.post('/', createProduct);
+router.post('/', validateData(createProductSchema), createProduct);
 
-router.patch('/:id', updateProduct);
+router.patch('/:id', validateData(updateProductSchema), updateProduct);
 
 router.delete('/:id', deleteProduct);
 
